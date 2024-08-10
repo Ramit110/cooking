288dbd5ae7f89743e45602d0e131d95019b77935
ramit110 <Ramit110@users.noreply.github.com>
2024-08-11 00:32:01 +0100

    first attempt

--- js/get_recipe.js
+++ js/get_recipe.js
@@ -1,4 +1,28 @@
 
 get_recipe = function (recipe_name) {
-    const URL = `${Window.location.origin}/recipes/${recipe_name}.json`;
+    const url = `${Window.location.origin}/recipes/${recipe_name}.json`;
+    try {
+        const response = fetch(url);
+        if (!response.ok) {
+            const new_tr = document.createElement("tr");
+            const name = document.createElement("th");
+            new_tr.append(name);
+            const tools = document.createElement("th");
+            new_tr.append(tools);
+            const tags = document.createElement("th");
+            new_tr.append(tags);
+            const ingredients = document.createElement("th");
+            new_tr.append(ingredients);
+            const recipe = document.createElement("th");
+            new_tr.append(recipe);
+          postMessage(new_tr);
+        }
+    
+        const json = response.json();
+        console.log(json);
+        postMessage(null);
+      } catch (error) {
+        console.error(error.message);
+        postMessage(null);
+      }
 }

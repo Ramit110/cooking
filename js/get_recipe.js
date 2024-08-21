9cc92470b6b3091e5213f7051a2580e5ee6995bb
ramit110 <Ramit110@users.noreply.github.com>
2024-08-21 19:34:55 +0100

    message != functions

--- js/get_recipe.js
+++ js/get_recipe.js
@@ -1,30 +1,32 @@
 
-onmessage = async function (document, recipe_name) {
-    try {
-        console.error(recipe_name);
-        const response = await fetch(recipe_name);
-        if (!response.ok) {
-          this.postMessage(document.createElement("tr"));
-        }
+onmessage = (message) => {
+  document = message.data[0]
+  recipe_name = message.data[1]
+  try {
+    console.log(recipe_name);
+    const response = fetch(recipe_name);
+    if (!response.ok) {
+      this.postMessage(document.createElement("tr"));
+    }
 
-        const json = response.json();
-        console.log(json);
+    const json = response.json();
+    console.log(json);
 
-        const new_tr = document.createElement("tr");
-        const name = document.createElement("th");
-        new_tr.append(name);
-        const tools = document.createElement("th");
-        new_tr.append(tools);
-        const tags = document.createElement("th");
-        new_tr.append(tags);
-        const ingredients = document.createElement("th");
-        new_tr.append(ingredients);
-        const recipe = document.createElement("th");
-        new_tr.append(recipe);
-        postMessage(new_tr);
+    const new_tr = document.createElement("tr");
+    const name = document.createElement("th");
+    new_tr.append(name);
+    const tools = document.createElement("th");
+    new_tr.append(tools);
+    const tags = document.createElement("th");
+    new_tr.append(tags);
+    const ingredients = document.createElement("th");
+    new_tr.append(ingredients);
+    const recipe = document.createElement("th");
+    new_tr.append(recipe);
+    postMessage(new_tr);
 
-      } catch (error) {
-        console.error(error.message);
-        postMessage(document.createElement("tr"));
-      }
+  } catch (error) {
+    console.error(error.message);
+    postMessage(document.createElement("tr"));
+  }
 }

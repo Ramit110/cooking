e06ccd29cb045d1cf4492558d706a576126bcef8
Ramit110 <Ramit110@users.noreply.github.com>
2024-08-21 22:46:56 +0100

    V0.1.0 (#10)
    * check the file
    
    * pass document to web worker
    
    * missed that file, also boom, multiple workers
    
    * message != functions
    
    * make it an array
    
    * ah yes, changes
    
    * fix url
    
    * our lord and saviour, async
    
    * promise to learn proomises?
    
    * no more promises, I can't be trusted
    
    * return of the promise
    
    * await this, await that
    
    * it works
    
    * almost there
    
    * work
    
    * check
    
    * message
    
    * joins
    
    * dict is not as good as ***list***
    
    * update template
    
    * try this?

--- js/get_recipe.js
+++ js/get_recipe.js
@@ -1,29 +1,18 @@
 
-onmessage = async function (recipe_name) {
-    try {
-        const response = await fetch(recipe_name);
-        if (!response.ok) {
-          this.postMessage(document.createElement("tr"));
-        }
+onmessage = async (message) => {
+  const request = new Request(message.data);
+  try {
+    const response = await fetch(request);
+    if (!response.ok) {
+      console.error(response);
+      this.postMessage(null);
+    }
 
-        const json = response.json();
-        console.log(json);
-
-        const new_tr = document.createElement("tr");
-        const name = document.createElement("th");
-        new_tr.append(name);
-        const tools = document.createElement("th");
-        new_tr.append(tools);
-        const tags = document.createElement("th");
-        new_tr.append(tags);
-        const ingredients = document.createElement("th");
-        new_tr.append(ingredients);
-        const recipe = document.createElement("th");
-        new_tr.append(recipe);
-        postMessage(new_tr);
-
-      } catch (error) {
-        console.error(error.message);
-        postMessage(document.createElement("tr"));
-      }
+    const json = await response.json();
+    postMessage(json);
+  }
+  catch (error) {
+    console.error(error.message);
+    postMessage(null);
+  };
 }
--- js/read_recipes.js
+++ js/read_recipes.js
@@ -1,17 +1,53 @@
 
+window.onload = function() {
+    LoadTable();
+};
+
 /**
  * List of recipes (json files in `recipes/` of this repository) and the webworker used to load
  * those recipes.
  */
 const recipes = ["sponge_cake"];
-const worker = new Worker('js/get_recipe.js');
-worker.onmessage = function(new_element) {
-    document.getElementById("CookingTable").appendChild(new_element);
-};
 
 /**
  * Load the table with all the recipe info, uses web workers and async calls to update the table.
  */
 function LoadTable() {
-    rows = recipes.forEach((file) => worker.postMessage(`${window.location.origin}/recipes/${file}.json`));
+    rows = recipes.forEach((file) => {
+        const worker = new Worker('js/get_recipe.js');
+        worker.onmessage = function(message) {
+            const new_element = message.data;
+            if (new_element === null) {
+                console.error("Nice");
+            }
+            else
+            {
+                const new_tr = document.createElement("tr");
+
+                const name = document.createElement("th");
+                name.innerHTML = new_element['name'];
+                new_tr.append(name);
+
+                const tools = document.createElement("th");
+                tools.innerHTML = new_element['tools'].join('<br/>');
+                new_tr.append(tools);
+
+                const tags = document.createElement("th");
+                tags.innerHTML = new_element['tags'].join('<br/>');
+                new_tr.append(tags);
+
+                const ingredients = document.createElement("th");
+                ingredients.innerHTML = new_element['ingredients'].join('<br/>');
+                new_tr.append(ingredients);
+
+                const recipe = document.createElement("th");
+                recipe.innerHTML = new_element['recipe'].join('<br/>');
+                new_tr.append(recipe);
+
+                document.getElementById("CookingTable").appendChild(new_tr);
+            }
+
+        };
+        worker.postMessage(`${window.location.origin}/cooking/recipes/${file}.json`);
+    });
 }
